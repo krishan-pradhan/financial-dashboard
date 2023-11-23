17ce54fe481787f85be04997fba02b551a2096db
Krishan Pradhan <krishan@quadranttech.co>
2023-11-23 21:31:37 +0530

    new changes

---
+++ nextjs-dashboard/app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardSkeleton from '../../ui/skeletons'
+
+const Loading = () => {
+  return (
+    <>
+    <DashboardSkeleton/>
+    </>
+  )
+}
+
+export default Loading
